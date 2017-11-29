537a46ac13890f9a87f86ad05a89028eb9b441d9
Grantrd <grantr@berea.edu>
2017-11-28 19:00:57 -0500

    yo

---
+++ story.py
@@ -0,0 +1,63 @@
+from tkinter import *
+root = Tk()
+T = text(root, height = 2, width = 30)
+T.pack()
+T.insert(END, "just a text Widget\nin two lines\n")
+mainloop()
+
+    wn = turtle.Screen()#makes a turtle screen
+    turtle.penup()
+    turtle.forward(50)
+    turtle.goto(-16, -6)  # Sets the location of the upc code
+    turtle.color("black")
+    turtle.write(font=("Arial", 15, "normal"))  # writes the upc code
+    turtle.hideturtle()
+    wn.exitonclick()
+class story:
+"""rough bones of the story probably to be output as turtle text"""
+
+gender = " " + "man" + " "
+print("long ago in a land... somewhere, there was a" + gender + "from a small village who, out one day fell into")
+print("a cave !!!")
+print("that's it")
+print("As the player awakes from the cave they hear a voice, the narrator")
+print("Narrator: Greetings young player, welcome to my cave, and your new home")
+print("for")
+print("forever")
+print("\n")
+print("Narrator: you young ")
+print("H")
+print("E")
+print("R")
+print("O")
+print("\n")
+print("have 3 options if you choose to attempt to escape from your new home ")
+print("and one if you choose to be sensible, and stay, with me, and become one of my cave dwellers")
+print("so which do you choose, ")
+weapon = input("so young hero you have chosen to oppose me, very well then, I am a gracious host, so choose your weapon, " \
+         "sword, staff or shield, ")
+"""
+if(user_input == left path)
+
+print("well young hero , you choose to oppose me, then fine, you chose this path, so you shall suffer it's consequences, the path of fire") 
+ 
+
+if(user_input == right path)
+
+if(user_input == forward path)
+
+if(user_input == stay here)
+    print("ah so you do see sense, well then welcome my new champion, to your new home")""
+    visuals
+
+
+
+
+
+
+
+
+
+
+
+"""
