da68437f1f8d1d8c4e420f9b5cb2284d243e3f7c
Grantrd <grantr@berea.edu>
2017-11-30 16:46:43 -0500

    yo

--- Game_class.py
+++ Game_class.py
@@ -1,9 +1,27 @@
 import pygame
 
+"""setup Screen"""
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+display_width = 800
+display_height = 600
+"""Colors"""
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+"""setup Game"""
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
+heroimg = pygame.image.load('hero.gif')
+
+def hero(x, y):
+    gameDisplay.blit(heroimg, (x, y))
+
+x = (display_width  * 0.45)
+y = (display_height * 0.80)
 crashed = False
 while not crashed:
 
@@ -11,8 +29,10 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
         print(event)
-    pygame.display.update()
 
+    gameDisplay.fill(white)
+    hero(x, y)
+    pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
