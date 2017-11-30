27cec35d273b92983a3ec43c5516126fc888ee32
Grantrd <grantr@berea.edu>
2017-11-30 17:35:30 -0500

    yo

--- Game_class.py
+++ Game_class.py
@@ -20,12 +20,15 @@ heroimg = pygame.image.load('hero.gif')
 def hero(x, y):
     gameDisplay.blit(heroimg, (x, y))
 
+"""starting x, and y values for hero, and starting x and y change values"""
+floor = int(display_height * 0.80)
 x = (display_width  * 0.45)
-y = (display_height * 0.80)
-
+y = floor
 x_change = 0
+y_change = 0
 
 crashed = False
+
 while not crashed:
 
     for event in pygame.event.get():
@@ -38,11 +41,17 @@ while not crashed:
                 x_change = -5
             if event.key == pygame.K_RIGHT:
                 x_change = 5
+            if event.key == pygame.K_SPACE:
+                y_change = -10
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
+            if event.key == pygame.K_SPACE & y < floor:
+                y_change = -1
 
     x += x_change
+    y += y_change
     gameDisplay.fill(white)
     hero(x, y)
     pygame.display.update()
